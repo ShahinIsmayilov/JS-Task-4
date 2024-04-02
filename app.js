fed5e7fdb23771d1abb3c1badfbb962fac5226c4
Şahin İsmayılov <157419313+ismylowss@users.noreply.github.com>
2024-04-02 23:42:25 +0400

    JS

---
+++ app.js
@@ -0,0 +1,24 @@
+// "Task 1"
+// var products = [
+//     { Id: "Laptop", Price: 2500 },  
+//     { Id: "Iphone", Price: 1500 },
+//     { Id: "Airpods", Price: 300 },
+//     { Id: "Keyboard", Price: 200 },
+//     { Id: "Mouse", Price: 100 }
+// ];
+// products.sort((a, b) => a.Price - b.Price);
+// for (var i = 0; i < products.length; i++) {
+//     console.log(`${products[i].Id}: ${products[i].Price} $`);
+// }
+
+////////////////////////////////////////////////////////////////
+
+// "Task 2"
+// var cem = 0;
+// for (var i = 0; i < 5; i++) {
+//     var randomsayi = parseInt(Math.random() * 101);
+//     console.log("Random sayi:", randomsayi);
+//     cem += randomsayi;
+// }
+
+// console.log("Sayilarin cemi:", cem);
